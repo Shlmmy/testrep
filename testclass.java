52240056217b2d3de011f959d350dd459a6252b3
Shlmmy <shimmy@olympus.ru>
2013-06-27 23:23:08 +0600

    Update testclass.java

--- testclass.java
+++ testclass.java
@@ -1,6 +1,12 @@
 package testpack;
 public class testclass {
   public static void main(String[] args) {
-		System.out.println("Hello user #2");
+		double leib = 0;
+        for (int i = 0; i < 200; i++)
+        {
+            leib = leib + Math.pow(-1, i) / (2 * i + 1);
+        }
+        double pi = leib * 4;
+        System.out.println(pi);
 	}
 }
